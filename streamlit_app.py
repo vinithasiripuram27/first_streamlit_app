203f9b0c66efe9a4336614e93bf57a4809ef8eaa
Vinitha Siripuram <114945100+vinithasiripuram27@users.noreply.github.com>
2022-10-06 20:36:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# Create the repeatable code block (called a function)
+def get_fruityvice_date(this_fruit_choice):
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   # streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+   # take the json version of the response and normalize it
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+
 #New Section to display fruityvice API Response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -33,12 +41,8 @@ try:
    if not fruit_choice:
       streamlit.error("Please select a fruit to get information")
    else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      # streamlit.text(fruityvice_response.json()) #just writes the data to the screen
-      # take the json version of the response and normalize it
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      # output it to the screen as table
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_date(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLerror as e:
    stremlit.error()
 
